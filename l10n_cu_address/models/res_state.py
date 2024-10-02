c7a8c736be7ad875f539cd300c748f64da1bac38
Elier Ayala Bernal <elier@wclik.com>
2024-10-02 15:57:19 -0400

    [FIX][17.0] Cambiar la relación de municipios de Many2Many a One2Many
    - Se ajustó el campo  para establecer una relación One2Many entre estados/provincias y municipios.
    - Ahora, cada municipio pertenece exclusivamente a una provincia/estado, lo que mejora la integridad de los datos y la gestión de las relaciones.

--- l10n_cu_address/models/res_state.py
+++ l10n_cu_address/models/res_state.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields 
+from odoo import models, fields
+
 
 class State(models.Model):
     _inherit = 'res.country.state'
 
-    res_municipality_id = fields.Many2one('res.municipality', 'Municipio', help="Municipios de Cuba")
+    res_municipality_id = fields.One2many('res.municipality', 'state_id', 'Municipio', help="Municipios de Cuba")
